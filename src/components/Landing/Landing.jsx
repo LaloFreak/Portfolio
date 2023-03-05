6f7fa6ba73d3250388cc6992546f36b8922cf190
Lalo Freak <lalo.gwerh@gmail.com>
2023-03-05 20:38:26 -0300

    thrash

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
 import s from './css/Landing.module.css'
+import { Profile } from '../Profile/Profile'
 
 export const Landing = () => {
   return (
     <div className={s.landingCont}>
+      <Profile />
       <Link to='/home'>
-        <div className={s.enterBtn}>Enter</div>
+        <div className={s.enterBtn}>enter</div>
       </Link>
     </div>
   )
