add6b56c0b1e2b25f753640604e608caefd953e6
Lalo Freak <lalo.gwerh@gmail.com>
2023-03-08 23:04:33 -0300

    thrash

--- src/components/Options/OptionsCanvas.jsx
+++ src/components/Options/OptionsCanvas.jsx
@@ -26,46 +26,59 @@ export const OptionCanvas = () => {
             <><Home/>
             <div className={s.btnsCont}>
                 <img src='true' alt='' />
-                <img 
-                    className={s.menuback} 
-                    onClick={()=>{return dispatch(setOption('bio'))}}
-                    src={arrowIcon} 
-                    alt='next' 
-                    width='30px'  
-            />
+                <div style={{display:'flex'}}>
+                    <h3 style={{fontFamily: 'Trajan', fontSize:'18px', marginRight:'10px'}}>bio</h3>
+                    <img 
+                        className={s.menuback} 
+                        onClick={()=>{return dispatch(setOption('bio'))}}
+                        src={arrowIcon} 
+                        alt='next' 
+                        width='30px'  
+                    />
+                </div>
             </div></>
             :null}
             {option==='bio'?
             <><Bio/>
+
             <div className={s.btnsCont}>
-                <img 
-                  className={s.menuback} 
-                  onClick={()=>{return dispatch(setOption('programation, design & sound'))}} 
-                  src={arrowIcon} 
-                  alt='next' 
-                  width='30px'
-                  style={{rotate: '180deg'}}
-                />
-                <img 
-                  className={s.menuback} 
-                  onClick={()=>{return dispatch(setOption('cv'))}} 
-                  src={arrowIcon} 
-                  alt='next' 
-                  width='30px'
-                />
+                <div style={{display:'flex'}}>
+                    <img 
+                    className={s.menuback} 
+                    onClick={()=>{return dispatch(setOption('programation, design & sound'))}} 
+                    src={arrowIcon} 
+                    alt='next' 
+                    width='30px'
+                    style={{rotate: '180deg'}}
+                    />
+                    <h3 style={{fontFamily: 'Trajan', fontSize:'18px', marginLeft:'10px'}}>home</h3>
+                </div>
+                <div style={{display:'flex'}}>
+                    <h3 style={{fontFamily: 'Trajan', fontSize:'18px', marginRight:'10px'}}>cv</h3>
+                    <img 
+                    className={s.menuback} 
+                    onClick={()=>{return dispatch(setOption('cv'))}} 
+                    src={arrowIcon} 
+                    alt='next' 
+                    width='30px'
+                    />
+                </div>
             </div></>
             :null}
             {option==='cv'?
             <><Cv/>
             <div className={s.btnsCont}>
-                <img 
-                    className={s.menuback} 
-                    onClick={()=>{return dispatch(setOption('bio'))}}
-                    src={arrowIcon} 
-                    alt='next' 
-                    width='30px'
-                    style={{rotate: '180deg'}}  
-                />
+                <div style={{display:'flex'}}>
+                    <img 
+                        className={s.menuback} 
+                        onClick={()=>{return dispatch(setOption('bio'))}}
+                        src={arrowIcon} 
+                        alt='next' 
+                        width='30px'
+                        style={{rotate: '180deg'}}  
+                    />
+                    <h3 style={{fontFamily: 'Trajan', fontSize:'18px', marginLeft:'10px'}}>bio</h3>
+                </div>
             </div></>
             :null}
         </div>
